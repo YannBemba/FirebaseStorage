30c67649c1d1d1c72fe4f08fe605429d7bae8ac5
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-02-07 20:03:03 +0100

    Ajout des images

--- app/src/main/java/com/example/firebasestorage/MainActivity.kt
+++ app/src/main/java/com/example/firebasestorage/MainActivity.kt
@@ -1,16 +1,80 @@
 package com.example.firebasestorage
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebasestorage.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        private const val REQUEST_CODE_IMAGE_PICK = 0
+    }
+
+    // Référence à l'emplacement de stockage de fichiers
+    private val imageRef = Firebase.storage.reference
+
+    private var curFile: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val ivImage = binding.ivImage
+        ivImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, REQUEST_CODE_IMAGE_PICK)
+            }
+        }
+
+        val btnUploadImg = binding.btnUploadImage
+        btnUploadImg.setOnClickListener {
+            uploadImageToStorage("mon_image")
+        }
+
     }
+
+    private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            curFile?.let {
+                imageRef.child("images/$filename").putFile(it).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Ajout de l'image réussi", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        } catch(e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && resultCode == REQUEST_CODE_IMAGE_PICK) {
+            data?.data?.let {
+                curFile = it
+                binding.ivImage.setImageURI(it)
+            }
+        }
+    }
+
+
+
 }
